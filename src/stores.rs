eb5e9f720ab26549b3ec3f6742fd4dcb9ffd60d2
writemorecode <gustav.ae.karlsson@gmail.com>
2025-01-26 19:06:30 +0100

    Cleanup of stores code

--- src/stores.rs
+++ src/stores.rs
@@ -32,40 +32,58 @@ pub struct StoreResponse {
 }
 
 #[instrument(skip(db))]
+async fn find_store_by_address(
+    db: &PgPool,
+    store: &Store,
+) -> Result<Option<StoreResponse>, ApiError> {
+    let store_id = sqlx::query_as!(
+        StoreResponse,
+        "SELECT id FROM stores WHERE street_number = $1 AND street_name = $2 AND city = $3 AND country_code = $4",
+        store.street_number,
+        store.street_name,
+        store.city,
+        store.country_code
+    )
+    .fetch_optional(db)
+    .await?;
+    Ok(store_id)
+}
+
+#[instrument(skip(db))]
+async fn insert_store(db: &PgPool, store: &Store) -> Result<StoreResponse, ApiError> {
+    sqlx::query_as!(
+        StoreResponse,
+        r#"
+        INSERT INTO stores (name, street_number, street_name, city, country_code, coordinate)
+        VALUES ($1,$2,$3,$4,$5,
+                ST_SetSRID(ST_MakePoint($6, $7), 4326)
+        )
+        RETURNING id
+        "#,
+        store.name,
+        store.street_number,
+        store.street_name,
+        store.city,
+        store.country_code,
+        store.longitude,
+        store.latitude,
+    )
+    .fetch_one(db)
+    .await
+    .map_err(Into::into)
+}
+
+#[instrument(skip(db))]
 pub async fn add_store(
     State(db): State<PgPool>,
     Json(store): Json<Store>,
 ) -> Result<Json<StoreResponse>, ApiError> {
-    let store_id = sqlx::query_as!(StoreResponse,
-        "SELECT id FROM stores WHERE street_number = $1 AND street_name = $2 AND city = $3 AND country_code = $4",
-        store.street_number, store.street_name, store.city, store.country_code)
-        .fetch_optional(&db)
-        .await?;
-
-    if let Some(store) = store_id {
-        return Ok(Json(store));
-    } else {
-        let store_id = sqlx::query_as!(
-            StoreResponse,
-            r#"
-            INSERT INTO stores (name, street_number, street_name, city, country_code, coordinate)
-            VALUES ($1,$2,$3,$4,$5,
-                    ST_SetSRID(ST_MakePoint($6, $7), 4326)
-            )
-            RETURNING id
-            "#,
-            store.name,
-            store.street_number,
-            store.street_name,
-            store.city,
-            store.country_code,
-            store.longitude,
-            store.latitude,
-        )
-        .fetch_one(&db)
-        .await?;
-        Ok(Json(store_id))
+    if let Some(store_id) = find_store_by_address(&db, &store).await? {
+        return Ok(Json(store_id));
     }
+
+    let store_id = insert_store(&db, &store).await?;
+    Ok(Json(store_id))
 }
 
 #[instrument(skip(db))]
